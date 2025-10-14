94e8b81bc39e3c43da5677df057abf50dc298b53
Immanuel Joy <immanueljoy107@gmail.com>
2025-10-14 11:25:54 -0400

    updated intendation

--- playgenerator.py
+++ playgenerator.py
@@ -109,11 +109,11 @@ data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                                  batch_input_shape=[batch_size, None]),
+                                batch_input_shape=[batch_size, None]),
         tf.keras.layers.LSTM(rnn_units,
-                             return_sequences=True,
-                             stateful=True,
-                             recurrent_initializer='glorot_uniform'),
+                            return_sequences=True,
+                            stateful=True,
+                            recurrent_initializer='glorot_uniform'),
         tf.keras.layers.Dense(vocab_size)
     ])
     return model
